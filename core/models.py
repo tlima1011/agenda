9dd8687c3bf8fd2b0ab66bb384e1b764c3940f23
Thiago <thiagolima1011@gmail.com>
2021-10-15 12:36:24 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
@@ -14,3 +16,6 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H %MHrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from core.models import Evento
+
+
+def evento(request, evento):
+    e = Evento.get(evento=evento)
+    return HttpResponse('{}', format(e))
+
+'''
+def index(request):
+    return redirect('/agenda/')
+'''
+
+
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
